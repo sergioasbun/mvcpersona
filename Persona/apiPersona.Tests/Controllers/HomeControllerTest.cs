045b9db3909bc37e65fed5557c51993793747ea6
sergio asbun fernandez <a2016114282@estudiantes.upsa.edu.bo>
2021-03-06 09:32:27 -0400

    proyecto base web api

---
+++ Persona/apiPersona.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using apiPersona;
+using apiPersona.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace apiPersona.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Disponer
+            HomeController controller = new HomeController();
+
+            // Actuar
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Declarar
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ Persona/apiPersona/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace apiPersona.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
